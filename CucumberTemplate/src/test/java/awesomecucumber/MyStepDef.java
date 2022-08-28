d92af3ce172ea9250a37e8e5d2f11a2be3fb8040
Moulali Syed <syedmoulali1997@gmail.com>
2022-08-28 19:53:53 +0530

    gherkin

--- CucumberTemplate/src/test/java/awesomecucumber/MyStepDef.java
+++ CucumberTemplate/src/test/java/awesomecucumber/MyStepDef.java
@@ -6,21 +6,58 @@ import io.cucumber.java.en.When;
 
 public class MyStepDef {
 
-	@Given("I'm dummy")
-	public void i_m_dummy() {
+	@Given("I'm on the Home page and I navigate to Store Page by pressing {string} menu option")
+	public void i_m_on_the_home_page_and_i_navigate_to_store_page_by_pressing_menu_option(String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
 
-	@When("I do dummy things")
-	public void i_do_dummy_things() {
+	@When("I click {string} button next to the product {string}")
+	public void i_click_button_next_to_the_product(String string, String string2) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("dummy things happen")
-	public void dummy_things_happen() {
+	@Then("the {string} button changes and a tick mark is shown on it")
+	public void the_button_changes_and_a_tick_mark_is_shown_on_it(String string) {
 		// Write code here that turns the phrase above into concrete actions
 		throw new io.cucumber.java.PendingException();
 	}
+
+	@Then("{string} link is shown")
+	public void link_is_shown(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on the {string} link")
+	public void i_click_on_the_link(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I'm navigated to the Cart page")
+	public void i_m_navigated_to_the_cart_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I fetch the product name and quantity")
+	public void i_fetch_the_product_name_and_quantity() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the product name is {string}")
+	public void the_product_name_is(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the product quantity is {int}")
+	public void the_product_quantity_is(Integer int1) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
 }
